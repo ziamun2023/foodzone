27f2e6c86a695dfa5e8c3be70d4a7588fac0d28a
Ziauddin Muntasir Chowdhury <ziauddinmuntasir587@gmail.com>
2023-12-21 14:53:17 +0600

    header section added

---
+++ src/app/components/layout/Header.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function Header () {
+    return (
+
+    <>
+    
+    <header className="flex items-center justify-between">
+        <Link className="text-primary font-semibold text-2xl" href="">food zone</Link>
+  
+        <nav className="flex gap-8 text-gray-500 font-semibold  items-center">
+        <Link href={''}>home</Link>
+        <Link href={''}>menu</Link>
+        <Link href={''}>about</Link>
+        <Link href={''}>contact</Link>
+        <Link href={''} className="bg-primary text-white px-8 py-2 rounded-full">login</Link>
+        </nav>
+      </header></>
+     
+    )
+
+}
